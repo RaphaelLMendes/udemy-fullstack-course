ff1429957ad1033023bbbd78b1b00fad72c62de2
Raphael Lucas Mendes <rapmendes@deloitte.com>
2021-10-27 17:34:33 -0300

    finish django_101

--- Django 101/My_app/mysite/settings.py
+++ Django 101/My_app/mysite/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = TEMPLATE_DIR = os.path.join(BASE_DIR,'media/')
+
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
     'feed',
 ]
 
